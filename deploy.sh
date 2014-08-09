8efaf696fe069893e40f4abbb2b3fd977a1484ce
Shawn Cook <cooksha@yahoo-inc.com>
2014-08-09 13:34:38 -0700

    Add version and deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+suffix=$1
+version=$2
+
+# Update version
+echo "\$( document ).ready(function(){\$('#version_text').html('Version:${version}');});" > ./html/version.js
+
+# Commit git
+git add -A
+git commit -m "ict_deploy_${suffix}_${version}"
+
+tarfileName="deploy_pkg_${suffix}_${version}.tgz"
+
+rm -rf *.tgz
+cd app
+mkdir -p "../../deploy_pkgs"
+tar czf "../../deploy_pkgs/$tarfileName" *
+cd ../
+
+ssh whitehel@whitehelmettech.com 'mkdir -p ~/deploy_pkgs'
+scp "../deploy_pkgs/$tarfileName" "whitehel@whitehelmettech.com:~/deploy_pkgs/"
+
+dirDestName="~/public_html/app_${suffix}_${version}"
+ssh whitehel@whitehelmettech.com "mkdir -p $dirDestName; rm -rf $dirDestName/*; tar xzf ~/deploy_pkgs/$tarfileName -C $dirDestName"
+
+linkName="~/public_html/app_${suffix}"
+ssh whitehel@whitehelmettech.com "rm -rf $linkName; ln -s $dirDestName $linkName"
