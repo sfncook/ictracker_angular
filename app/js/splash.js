3ef72771055c2ebd5a2cdfa6d249e15c720b422e
Shawn Cook <cooksha@yahoo-inc.com>
2014-12-21 17:56:53 -0800

    Splash buttons change to inc page

--- app/js/splash.js
+++ app/js/splash.js
@@ -3,6 +3,19 @@
 var app = angular.module("ictApp", []);
 
 app.controller('SplashCtrl', function($scope){
+
+    $scope.inc_num = "";
+    $scope.inc_add = "";
+
+    $scope.clickSplashBtn = function(incident_btn) {
+        localStorage.setItem('inc_num', $scope.inc_num);
+        localStorage.setItem('inc_add', $scope.inc_add);
+        localStorage.setItem('inc_typ', incident_btn.type);
+
+        var urlLink = "incident_form.html?inc_num="+$scope.inc_num;
+        window.location.href = urlLink;
+    };
+
     $scope.incident_start_btns = [
         {
             'hidden': true
