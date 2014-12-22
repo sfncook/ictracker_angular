e06127e6598b8acf3c9a4e98463f5e285c8950e3
Shawn Cook <cooksha@yahoo-inc.com>
2014-12-21 18:05:38 -0800

    inc_types.json

--- app/js/splash.js
+++ app/js/splash.js
@@ -2,7 +2,7 @@
 
 var app = angular.module("ictApp", []);
 
-app.controller('SplashCtrl', function($scope){
+app.controller('SplashCtrl', function($scope, $http){
 
     $scope.inc_num = "";
     $scope.inc_add = "";
@@ -16,68 +16,9 @@ app.controller('SplashCtrl', function($scope){
         window.location.href = urlLink;
     };
 
-    $scope.incident_start_btns = [
-        {
-            'hidden': true
-        },
-        {
-            'type': 'fire',
-            'icon': "img/icons/fire.png",
-            'text': "Fire Incident"
-        },
-        {
-            'type': 'medical',
-            'icon': "img/icons/medical.png",
-            'text': "Medical Incident"
-        },
-        {
-            'hidden': true
-        },
-        {
-            'type': 'arff',
-            'icon': "img/icons/plane.png",
-            'text': "ARFF Incident"
-        },
-        {
-//        type: 'hazmat',
-            'type': 'arff',
-            'icon': "img/icons/hazmat.png",
-            'text': "HazMat Incident"
-        },
-        {
-            'type': 'water',
-            'icon': "img/icons/water.png",
-            'text': "Water Rescue"
-        },
-        {
-//        type: 'trench',
-            'type': 'arff',
-            'icon': "img/icons/trench.png",
-            'text': "Trench Rescue"
-        },
-        {
-//        type: 'mountain',
-            'type': 'arff',
-            'icon': "img/icons/mountain.png",
-            'text': "Mountain Rescue"
-        },
-        {
-//        type: 'palm',
-            'type': 'arff',
-            'icon': "img/icons/palm.png",
-            'text': "Palm Rescue"
-        },
-        {
-//        type: 'struct',
-            'type': 'arff',
-            'icon': "img/icons/structure.png",
-            'text': "Structural Rescue"
-        },
-        {
-//        type: 'confined',
-            'type': 'arff',
-            'icon': "img/icons/confined.png",
-            'text': "Confined Space Rescue"
-        }
-    ];
+    $http.get('data/inc_types.json').
+        success(function(data){
+            $scope.incident_start_btns = data;
+        });
+
 });
