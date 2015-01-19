129116adb41a93a79b02063b09277903f31a5366
Shawn Cook <cooksha@yahoo-inc.com>
2015-01-19 08:58:32 -0800

    Save incident data on button click

--- app/js/splash.js
+++ app/js/splash.js
@@ -4,27 +4,49 @@ var app = angular.module("ictApp", ['ParseServices']);
 
 app.controller('SplashCtrl', function($scope, ParseObject, ParseQuery){
 
-    $scope.inc_num = "";
-    $scope.inc_add = "";
+    var IncidentParseObj = Parse.Object.extend('Incident');
+    $scope.incidentObj = new ParseObject(new IncidentParseObj(), ['inc_number','inc_address','inc_type']);
 
-    $scope.clickSplashBtn = function(incident_btn) {
-        localStorage.setItem('inc_num', $scope.inc_num);
-        localStorage.setItem('inc_add', $scope.inc_add);
-        localStorage.setItem('inc_typ', incident_btn.type);
-
-        var urlLink = "incident_form.html?inc_num="+$scope.inc_num;
-        window.location.href = urlLink;
-    };
-
+    // Incident Type Buttons
     var query = new Parse.Query(Parse.Object.extend('IncidentType'));
+    query.ascending("order");
     ParseQuery(query, {functionToCall:'find'}).then(function(result){
-//        $scope.newContact = new ParseObject(obj, ['firstName','lastName','email']);
         var data = new Array();
         for(var i=0; i<result.length; i++) {
             var obj = new ParseObject(result[i], ['type','icon','text']);
+            obj.rawParseObjId = result[i].id;
             data.push(obj);
         }
         $scope.inc_types = data;
     });
+
+    // Previous Incidents List
+    query = new Parse.Query(Parse.Object.extend('Incident'));
+//    query.ascending("order");
+    ParseQuery(query, {functionToCall:'find'}).then(function(result){
+        var data = new Array();
+        for(var i=0; i<result.length; i++) {
+            var type = result[i].get("inc_type");
+            type.fetch({
+                success: function(type) {
+                    console.log(type.get('icon')+" "+type.get('text')+" "+type.get('type'));
+                }
+            });
+        }
+//        $scope.inc_types = data;
+    });
+
+    // Respond to incident type button click
+    $scope.clickSplashBtn = function(incident_btn) {
+        var IncidentTypeParseObj = Parse.Object.extend("IncidentType");
+        var incidentTypeParseObj = new IncidentTypeParseObj();
+        incidentTypeParseObj.id = incident_btn.rawParseObjId;
+        $scope.incidentObj.type = incidentTypeParseObj;
+        $scope.incidentObj.save();
+
+//        var urlLink = "incident_form.html?inc_num="+$scope.inc_num;
+//        window.location.href = urlLink;
+    };
+
 
 });
