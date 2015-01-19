2f78cf91960833d67439a35c66feafe9012bc6df
Shawn Cook <cooksha@yahoo-inc.com>
2015-01-19 13:37:23 -0800

    Delete incidents with button click

--- app/js/splash.js
+++ app/js/splash.js
@@ -16,6 +16,25 @@ function fetchTypeForIncident(incident, $scope) {
     }
 }
 
+function loadIncidentList($scope, ParseObject, ParseQuery) {
+    // reset list
+    $scope.incident_list = new Array();
+
+    var query = new Parse.Query(Parse.Object.extend('Incident'));
+    ParseQuery(query, {functionToCall:'find'}).then(function(result){
+        var data = new Array();
+        for(var i=0; i<result.length; i++) {
+
+            var incident = new ParseObject(result[i], ['inc_number','inc_address','inc_type']);
+            incident.rawParseObj = result[i];
+            fetchTypeForIncident(incident, $scope);
+
+            data.push(incident);
+        }
+        $scope.incident_list = data;
+    });
+}
+
 app.controller('SplashCtrl', function($scope, ParseObject, ParseQuery){
 
     var IncidentParseObj = Parse.Object.extend('Incident');
@@ -28,34 +47,21 @@ app.controller('SplashCtrl', function($scope, ParseObject, ParseQuery){
     ParseQuery(query, {functionToCall:'find'}).then(function(result){
         var data = new Array();
         for(var i=0; i<result.length; i++) {
-            var obj = new ParseObject(result[i], ['type','icon','text']);
+            var obj = new ParseObject(result[i], ['nameLong','nameShort','icon']);
             obj.rawParseObjId = result[i].id;
             data.push(obj);
         }
         $scope.inc_types = data;
     });
 
-    // Previous Incidents List
-    $scope.incident_list = new Array();
-    query = new Parse.Query(Parse.Object.extend('Incident'));
-//    query.ascending("order");
-    ParseQuery(query, {functionToCall:'find'}).then(function(result){
-        var data = new Array();
-        for(var i=0; i<result.length; i++) {
-
-            var incident = new ParseObject(result[i], ['inc_number','inc_address','inc_type']);
-            fetchTypeForIncident(incident, $scope);
-
-            data.push(incident);
-        }
-        $scope.incident_list = data;
-    });
+    // Load incident list
+    loadIncidentList($scope, ParseObject, ParseQuery);
 
     // Respond to incident type button click
-    $scope.clickSplashBtn = function(incident_btn) {
+    $scope.clickIncidentTypeBtn = function(incidentType) {
         var IncidentTypeParseObj = Parse.Object.extend("IncidentType");
         var incidentTypeParseObj = new IncidentTypeParseObj();
-        incidentTypeParseObj.id = incident_btn.rawParseObjId;
+        incidentTypeParseObj.id = incidentType.rawParseObjId;
         $scope.incidentObj.type = incidentTypeParseObj;
 
         // Default value for inc_number
@@ -65,8 +71,27 @@ app.controller('SplashCtrl', function($scope, ParseObject, ParseQuery){
 
         $scope.incidentObj.save();
 
-//        var urlLink = "incident_form.html?inc_num="+$scope.inc_num;
+//        var urlLink = "incident_form.html?inc_num="+$scope.incidentObj.inc_number;
 //        window.location.href = urlLink;
+    };
+
+
+    $scope.loadIncident = function(incident) {
+    };
+
+    $scope.deleIncident = function(incident) {
+        var response = confirm("Are you sure you want to delete incident "+incident.inc_number+"?");
+        if (response == true) {
+            incident.rawParseObj.destroy({
+                success: function(myObject) {
+                    loadIncidentList($scope, ParseObject, ParseQuery);
+                },
+                error: function(myObject, error) {
+                    console.log("Error:"+error);
+                    loadIncidentList($scope, ParseObject, ParseQuery);
+                }
+            });
+        }
     };
 
 
