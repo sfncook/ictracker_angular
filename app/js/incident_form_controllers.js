5ff40d1e9e129d1982b6d0682abc45b2a7bd60bd
Shawn Cook <cooksha@yahoo-inc.com>
2015-02-18 08:40:40 -0800

    Remove commented code

--- app/js/incident_form_controllers.js
+++ app/js/incident_form_controllers.js
@@ -16,33 +16,6 @@ angular.module("ictApp", ['gridster', 'DataServices', 'TbarServices', 'ActionSer
         $scope.osrPerc = 0;
         $scope.objPerc = 0;
 
-        /*$scope.timer_text = "00:00";
-        $scope.hourRollOverDone = false;
-        var t0 = (new Date()).getTime();
-        function updateTimer() {
-            var t1 = (new Date()).getTime();
-            var elapsed = parseInt(t1-t0);
-            var elapsedSec = parseInt((elapsed/1000)%60);
-            var elapsedMin = parseInt((elapsed/(1000*60))%60);
-            var elapsedHr = parseInt((elapsed/(1000*60*60))%60);
-
-            var secStr = (elapsedSec<10)?("0"+elapsedSec):elapsedSec;
-            var minStr = (elapsedMin<10)?("0"+elapsedMin):elapsedMin;
-            var hrStr = (elapsedHr<10)?("0"+elapsedHr):elapsedHr;
-
-            var new_timer_text = "";
-            if (elapsedHr>0) {
-                if (!$scope.hourRollOverDone) {
-                    $scope.hourRollOverDone = true;
-                }
-                new_timer_text = hrStr+":"+minStr+":"+secStr;
-            } else {
-                new_timer_text = minStr+":"+secStr;
-            }
-            $scope.timer_text = new_timer_text;
-        }
-        $interval(updateTimer, 1000);*/
-
         $scope.showUnitsDlgForDispUnits = function() {
             DataStore.showUnitsDlgForDispUnits();
         }
