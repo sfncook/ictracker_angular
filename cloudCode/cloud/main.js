c5d2a519c6104cb917a2e4346faca103435ff44c
Shawn Cook <cooksha@yahoo-inc.com>
2015-02-19 20:36:36 -0800

    Parse.Cloud.afterSave Sector

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -4,3 +4,16 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.afterSave("Sector", function(request) {
+    query = new Parse.Query("Incident");
+    query.get(request.object.get("incident").id, {
+        success: function(incident) {
+            incident.increment("txid");
+            incident.save();
+        },
+        error: function(error) {
+            console.error("Got an error " + error.code + " : " + error.message);
+        }
+    });
+});
