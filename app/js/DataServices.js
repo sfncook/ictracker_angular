bd59696500f67eee3349a7cb6629dfbe6571a724
Shawn Cook <cooksha@yahoo-inc.com>
2015-02-24 18:50:38 -0800

    Init reports dialog

--- app/js/DataServices.js
+++ app/js/DataServices.js
@@ -7,6 +7,7 @@ var UNIT_TYPE_DEF = ['name', 'type', 'city'];
 var UNIT_DEF = ['actions', 'manyPeople', 'manyPar', 'par', 'psi', 'sector', 'type'];
 var ACTION_TYPE_DEF = ['name', 'category', 'incidentType', 'isWarning'];
 var MAYDAY_DEF = ['incident', 'number', 'unitType', 'sectorType', 'isOnHoseline', 'isUnInjured', 'isLost', 'isTrapped', 'isOutOfAir', 'isRegulatorIssue', 'isLowAir', 'isPackIssue', 'nameFFighter', 'psi', 'channel', 'rank'];
+var REPORT_DEF = ['text'];
 
 angular.module('DataServices', ['ParseServices'])
 
