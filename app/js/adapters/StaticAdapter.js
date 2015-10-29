c2c3976978ed4bb7f8be526a2b089d65a8c567c7
Shawn Cook <cooksha@yahoo-inc.com>
2015-10-28 17:35:44 -0700

    LoadIncidentTypes_Static

--- app/js/adapters/StaticAdapter.js
+++ app/js/adapters/StaticAdapter.js
@@ -1,10 +1,10 @@
 angular.module('StaticAdapter', [])
 
-    .factory('StaticAdapter', function(isLoggedIn_Static) {
+    .factory('StaticAdapter', function(LoadIncidentTypes_Static, isLoggedIn_Static) {
         return {
             adapter_id_str:'static',
             init:function(){console.log("StaticAdapter init");},
-            LoadIncidentTypes: function(){console.log("StaticAdapter LoadIncidentTypes");},
+            LoadIncidentTypes: LoadIncidentTypes_Static,
             LoadAllIncidents: function(){console.log("StaticAdapter LoadAllIncidents");},
             LoadIncident: function(){console.log("StaticAdapter LoadIncident");},
             UpdateIncidentAsNeeded: function(){console.log("StaticAdapter UpdateIncidentAsNeeded");},
@@ -15,6 +15,69 @@ angular.module('StaticAdapter', [])
     .factory('isLoggedIn_Static', function () {
         return function () {
             return true;
+        }
+    })
+
+    .factory('LoadIncidentTypes_Static', function ($q) {
+        return function () {
+            var inc_types = [
+                {
+                    "type": "fire",
+                    "icon": "img/icons/fire.png",
+                    "text": "Fire Incident"
+                },
+                {
+                    "type": "medical",
+                    "icon": "img/icons/medical.png",
+                    "text": "Medical Incident"
+                },
+                {
+                    "hidden": true
+                },
+                {
+                    "type": "arff",
+                    "icon": "img/icons/plane.png",
+                    "text": "ARFF Incident"
+                },
+                {
+                    "type": "hazmat",
+                    "icon": "img/icons/hazmat.png",
+                    "text": "HazMat Incident"
+                },
+                {
+                    "type": "water",
+                    "icon": "img/icons/water.png",
+                    "text": "Water Rescue"
+                },
+                {
+                    "type": "trench",
+                    "icon": "img/icons/trench.png",
+                    "text": "Trench Rescue"
+                },
+                {
+                    "type": "mountain",
+                    "icon": "img/icons/mountain.png",
+                    "text": "Mountain Rescue"
+                },
+                {
+                    "type": "palm",
+                    "icon": "img/icons/palm.png",
+                    "text": "Palm Rescue"
+                },
+                {
+                    "type": "struct",
+                    "icon": "img/icons/structure.png",
+                    "text": "Structural Rescue"
+                },
+                {
+                    "type": "confined",
+                    "icon": "img/icons/confined.png",
+                    "text": "Confined Space Rescue"
+                }
+            ];
+
+            var promise = $q.when(inc_types);
+            return promise;
         }
     })
 ;
