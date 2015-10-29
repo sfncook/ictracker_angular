73c04af99f33dc6f230401a3907c445ee801c7c7
Shawn Cook <cooksha@yahoo-inc.com>
2015-10-28 17:45:21 -0700

    LoadAllIncidents_Static and fix bugs with incTypes

--- app/js/adapters/StaticAdapter.js
+++ app/js/adapters/StaticAdapter.js
@@ -1,11 +1,11 @@
 angular.module('StaticAdapter', [])
 
-    .factory('StaticAdapter', function(LoadIncidentTypes_Static, isLoggedIn_Static) {
+    .factory('StaticAdapter', function(LoadIncidentTypes_Static, LoadAllIncidents_Static, isLoggedIn_Static) {
         return {
             adapter_id_str:'static',
             init:function(){console.log("StaticAdapter init");},
             LoadIncidentTypes: LoadIncidentTypes_Static,
-            LoadAllIncidents: function(){console.log("StaticAdapter LoadAllIncidents");},
+            LoadAllIncidents: LoadAllIncidents_Static,
             LoadIncident: function(){console.log("StaticAdapter LoadIncident");},
             UpdateIncidentAsNeeded: function(){console.log("StaticAdapter UpdateIncidentAsNeeded");},
             isLoggedIn: isLoggedIn_Static
@@ -20,64 +20,90 @@ angular.module('StaticAdapter', [])
 
     .factory('LoadIncidentTypes_Static', function ($q) {
         return function () {
-            var inc_types = [
-                {
-                    "type": "fire",
-                    "icon": "img/icons/fire.png",
-                    "text": "Fire Incident"
-                },
-                {
-                    "type": "medical",
-                    "icon": "img/icons/medical.png",
-                    "text": "Medical Incident"
-                },
-                {
-                    "hidden": true
-                },
-                {
-                    "type": "arff",
-                    "icon": "img/icons/plane.png",
-                    "text": "ARFF Incident"
-                },
-                {
-                    "type": "hazmat",
-                    "icon": "img/icons/hazmat.png",
-                    "text": "HazMat Incident"
-                },
-                {
-                    "type": "water",
-                    "icon": "img/icons/water.png",
-                    "text": "Water Rescue"
-                },
-                {
-                    "type": "trench",
-                    "icon": "img/icons/trench.png",
-                    "text": "Trench Rescue"
-                },
-                {
-                    "type": "mountain",
-                    "icon": "img/icons/mountain.png",
-                    "text": "Mountain Rescue"
-                },
-                {
-                    "type": "palm",
-                    "icon": "img/icons/palm.png",
-                    "text": "Palm Rescue"
-                },
-                {
-                    "type": "struct",
-                    "icon": "img/icons/structure.png",
-                    "text": "Structural Rescue"
-                },
-                {
-                    "type": "confined",
-                    "icon": "img/icons/confined.png",
-                    "text": "Confined Space Rescue"
-                }
-            ];
+            var promise = $q.when(INC_TYPES);
+            return promise;
+        }
+    })
 
-            var promise = $q.when(inc_types);
+    .factory('LoadAllIncidents_Static', function ($q) {
+        return function () {
+            var promise = $q.when(INCIDENTS);
             return promise;
         }
     })
 ;
+
+
+var INC_TYPES = [
+    {
+        "type": "fire",
+        "icon": "img/icons/fire.png",
+        "nameLong": "Fire Incident",
+        "order":1
+    },
+    {
+        "type": "medical",
+        "icon": "img/icons/medical.png",
+        "nameLong": "Medical Incident",
+        "order":2
+    },
+    {
+        "type": "arff",
+        "icon": "img/icons/plane.png",
+        "nameLong": "ARFF Incident",
+        "order":3
+    },
+    {
+        "type": "hazmat",
+        "icon": "img/icons/hazmat.png",
+        "nameLong": "HazMat Incident",
+        "order":4
+    },
+    {
+        "type": "water",
+        "icon": "img/icons/water.png",
+        "nameLong": "Water Rescue",
+        "order":5
+    },
+    {
+        "type": "trench",
+        "icon": "img/icons/trench.png",
+        "nameLong": "Trench Rescue",
+        "order":6
+    },
+    {
+        "type": "mountain",
+        "icon": "img/icons/mountain.png",
+        "nameLong": "Mountain Rescue",
+        "order":7
+    },
+    {
+        "type": "palm",
+        "icon": "img/icons/palm.png",
+        "nameLong": "Palm Rescue",
+        "order":8
+    },
+    {
+        "type": "struct",
+        "icon": "img/icons/structure.png",
+        "nameLong": "Structural Rescue",
+        "order":9
+    },
+    {
+        "type": "confined",
+        "icon": "img/icons/confined.png",
+        "nameLong": "Confined Space Rescue",
+        "order":10
+    }
+];
+
+var INCIDENTS = [
+    {
+        "id":"gFGa4HMohQ",
+        "json":"{\"inc_address\":\"add007\",\"inc_number\":\"inc007\",\"incidentType\":{\"icon\":\"img/icons/fire.png\",\"nameLong\":\"Fire Incident\",\"nameShort\":\"fire\",\"order\":1},\"iap\":{\"fireControl\": \"Shawn update 01 fire control\",\"firefighterSafety\": \"Shawn update 01 firefighter safety\",\"isActionEffect\": false,\"isArrangement\": false,\"isBuilding\": false,\"isFire\": true,\"isLifeHazard\": true,\"isOccupancy\": false,\"isResources\": true,\"isSpecial\": true,\"isSprinkler\": true,\"isVent\": true,\"propertyPeople\": \"Shawn update 01 property people\",\"rescue\": \"Shawn update 01 rescue\"}}"
+    },
+    {
+        "id":"mL9SQdUvPL",
+        "json":"{\"inc_address\":\"add008\",\"inc_number\":\"inc008\",\"incidentType\":{\"icon\":\"img/icons/hazmat.png\",\"nameLong\":\"HazMat Incident\",\"nameShort\":\"hazmat\",\"order\":1}}"
+    }
+];
