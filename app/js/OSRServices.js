fc7db6d1cf35de036da115cbe35224992b91abed
Shawn Cook <cooksha@yahoo-inc.com>
2015-11-07 22:04:59 -0800

    Clean up OSRServices.js

--- app/js/OSRServices.js
+++ app/js/OSRServices.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('OSRServices', ['ParseServices', 'DataServices'])
+angular.module('OSRServices', ['DataServices'])
 
     .controller('OsrDlg', function($scope, DataStore, UpdateOsrPercent){
         $scope.dataStore = DataStore;
@@ -55,58 +55,5 @@ angular.module('OSRServices', ['ParseServices', 'DataServices'])
             angular.element('#osr_perc_bar').css('width', ((amtChecked*100.0)/fullAmt) + '%');
         }
     })
-
-    .factory('LoadOSRForIncident', function (ConvertParseObject, DataStore, CreateNewOSR) {
-        return function (incident) {
-            var queryOSR = new Parse.Query(Parse.Object.extend('OSR'));
-            queryOSR.equalTo("incident", incident);
-            return queryOSR.first().then(
-                function(osrObject){
-                    if (osrObject){
-                        ConvertParseObject(osrObject, OSR_DEF);
-                        DataStore.osr = osrObject;
-                    } else {
-                        DataStore.osr = CreateNewOSR(incident);
-                    }
-                    DataStore.updateOSRPerc();
-                }
-            );
-        }
-    })
-
-    .factory('CreateNewOSR', ['ConvertParseObject', 'DataStore', function (ConvertParseObject, DataStore) {
-        return function (curIncident) {
-            var OSRParseObj = Parse.Object.extend('OSR');
-            var osrObject = new OSRParseObj();
-            ConvertParseObject(osrObject, OSR_DEF);
-            osrObject.isAddress = false;
-            osrObject.isOccupancy = false;
-            osrObject.isConstruction = false;
-            osrObject.isAssumeCommand = false;
-            osrObject.isLocation = false;
-            osrObject.isStrategy = false;
-            osrObject.isAttackLine = false;
-            osrObject.isWaterSupply = false;
-            osrObject.isIRIC = false;
-            osrObject.isBasement = false;
-            osrObject.isMobile = false;
-            osrObject.isDefensive = false;
-
-            osrObject.accountability = '';
-            osrObject.accountabilityLocation = '';
-            osrObject.unit = '';
-            osrObject.dispatchAddress = '';
-            osrObject.sizeOfBuilding = '';
-            osrObject.numberOfFloors = '';
-            osrObject.typeOfBuilding = '';
-            osrObject.subFloors = '';
-            osrObject.constructionType = '';
-            osrObject.roofType = '';
-            osrObject.conditions = '';
-
-            osrObject.incident=curIncident;
-            return osrObject;
-        }
-    }])
 
 ;
