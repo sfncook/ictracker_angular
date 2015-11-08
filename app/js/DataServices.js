24946ada8434eb7f1ebf6e3b97b11c6d9fd50822
Shawn Cook <cooksha@yahoo-inc.com>
2015-11-07 21:48:48 -0800

    Move parse class defs into ParseAdapter

--- app/js/DataServices.js
+++ app/js/DataServices.js
@@ -1,22 +1,4 @@
 
-var DEPARTMENT_DEF = ['name_short', 'name_long', 'app_key', 'js_key'];
-var INCIDENT_DEF = ['inc_number', 'inc_address', 'incidentType', 'inc_startDate', 'strategy', 'txid'];
-var INCIDENT_TYPE_DEF = ['icon', 'nameLong', 'nameShort', 'order'];
-var SECTOR_DEF = ['sectorType', 'direction', 'number', 'row', 'col', 'incident', 'acctUnit', 'bnchClsUnablePrim', 'bnchClsUnableSec', 'bnchCls1','bnchCls2','bnchCls3','bnchCls4','bnchVnt1','bnchVnt2','bnchVnt3','bnchIrc1','bnchIrc2','bnchIrc3','bnchIrc4','bnchSaf1','bnchSaf2','bnchTrt1','bnchTrt2','bnchTrt3','bnchLzo1','bnchLzo2','bnchLzo3','bnchTri1','bnchTri2','bnchTri3'];
-var SECTOR_TYPE_DEF = ['name', 'manyBenchmarkBars', 'hasAcctBtn', 'hasActions', 'hasClock', 'hasPsiBtn', 'isVisible', 'hasClassicBnch', 'hasVentBnch', 'hasIricBnch', 'hasSafetyBnch', 'hasTreatmentBnch', 'hasLzBnch', 'hasTriageBnch'];
-var UNIT_TYPE_DEF = ['name', 'type', 'city'];
-var UNIT_DEF = ['actions', 'manyPeople', 'manyPar', 'par', 'psi', 'sector', 'type'];
-var ACTION_TYPE_DEF = ['name', 'category', 'incidentType', 'isWarning'];
-var UPGRADE_DEF = ['incident', 'isWorkingFire', 'is1stAlarm', 'is2ndAlarm', 'is3rdAlarm', 'is4thAlarm', 'isBalanceTo', 'isEnRoute'];
-var MAYDAY_DEF = ['incident', 'number', 'unitType', 'sectorType', 'isOnHoseline', 'isUnInjured', 'isLost', 'isTrapped', 'isOutOfAir', 'isRegulatorIssue', 'isLowAir', 'isPackIssue', 'nameFFighter', 'psi', 'channel', 'rank'];
-var REPORT_ACTION_DEF = ['incident', 'sector', 'text'];
-var IAP_DEF = ['fireControl', 'firefighterSafety', 'incident', 'isActionEffect', 'isArrangement', 'isBuilding', 'isFire', 'isLifeHazard', 'isOccupancy', 'isResources', 'isSpecial', 'isSprinkler', 'isVent', 'propertyPeople', 'evacutionLocation', 'rescue'];
-var OSR_DEF = ['incident', 'isAddress', 'isOccupancy', 'isConstruction', 'isAssumeCommand', 'isLocation', 'isStrategy', 'isAttackLine', 'isWaterSupply', 'isIRIC', 'isBasement', 'isMobile', 'isDefensive', 'accountability', 'accountabilityLocation', 'unit', 'dispatchAddress', 'sizeOfBuilding', 'numberOfFloors', 'typeOfBuilding', 'subFloors', 'constructionType', 'roofType', 'conditions'];
-var OBJECTIVES_DEF = ['incident', 'upgradeToFullRescue', 'assingSafety', 'establishSupplyLine', 'secureUtilities', 'ventiliation', 'createOnDeck', 'pressurizeExposures', 'monitorChannel16', 'salvage', 'establishRehab', 'customerService'];
-var DISPATCHED_UNITS_DEF = ['incident', 'unitTypes'];
-var USER_DEF = ['username', 'email', 'name', 'department'];
-var ROLE_DEF = ['name', 'users', 'roles'];
-
 angular.module('DataServices', ['ParseServices', 'AdapterServices'])
     .factory('DefaultCity', function() {
         return "Mesa";
